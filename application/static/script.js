7ca2fcf9507a1e397bbfa1493fa0ee9a5e72c561
digoin <titouan.dechanet@teamwork.net>
2021-08-23 14:57:05 +0200

    fixed the issue where the page reload if you press enter and selecting the form

--- application/static/script.js
+++ application/static/script.js
@@ -1,3 +1,11 @@
+var input = document.getElementById("question");
+input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("validate").click();
+    }
+});
+
 let ask = async function() {
     question = document.getElementById("question").value
     let response = await fetch(`http://localhost:5000/question/${question}`)
