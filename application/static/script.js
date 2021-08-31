b905822bca88d36c6921604134e9fea167a7ad50
digoin <titouan.dechanet@teamwork.net>
2021-08-31 14:30:09 +0200

    implemented a gif when grandpy is thinking

--- application/static/script.js
+++ application/static/script.js
@@ -9,8 +9,10 @@ input.addEventListener("keyup", function(event) {
 let ask = async function() {
     question = document.getElementById("question").value
     const chat = document.getElementById("chat")
-    chat.innerHTML = `<p class="right">${question}</p>`+chat.innerHTML 
+    chat.innerHTML = `<p class="right">${question}</p>`+chat.innerHTML
+    chat_save = chat.innerHTML
+    chat.innerHTML = `<img class="dots" src="static/image/3dots.gif">`+chat_save
     let response = await fetch(`http://localhost:5000/question/${question}`)
     let data = await response.json()
-    chat.innerHTML = `<p class="left">${data["response"]}</p>`+ chat.innerHTML
+    chat.innerHTML = `<p class="left">${data["response"]}</p>`+ chat_save
 }
