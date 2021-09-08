83c477dc51736a17a4fbcde3014693fd4c3f1ebb
digoin <titouan.dechanet@teamwork.net>
2021-09-08 10:15:33 +0200

    map display well

--- application/static/script.js
+++ application/static/script.js
@@ -12,10 +12,8 @@ let ask = async function() {
     chat.innerHTML = `<p class="right">${question}</p>`+chat.innerHTML
     chat_save = chat.innerHTML
     chat.innerHTML = `<img class="dots" src="static/image/3dots.gif">`+chat_save
-    try {let response = await fetch(`http://localhost:5000/question/${question}`)}
-    catch (error) {
-        console.error(error)
-    }
+    let response = await fetch(`http://localhost:5000/question/${question}`)
+
     let data = await response.json()
     chat.innerHTML = `<p class="left">${data["response"]}</p>`+ chat_save
     chat.innerHTML = `<div id="map"></div>` + chat.innerHTML
@@ -27,6 +25,6 @@ let map;
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
+        zoom: 16,
   });
 }
