725b1d5a6990992cbdbc632d370a078b5f9ad7f5
digoin <titouan.dechanet@teamwork.net>
2021-09-08 08:40:36 +0200

    commit for checkout

--- application/static/script.js
+++ application/static/script.js
@@ -12,7 +12,21 @@ let ask = async function() {
     chat.innerHTML = `<p class="right">${question}</p>`+chat.innerHTML
     chat_save = chat.innerHTML
     chat.innerHTML = `<img class="dots" src="static/image/3dots.gif">`+chat_save
-    let response = await fetch(`http://localhost:5000/question/${question}`)
+    try {let response = await fetch(`http://localhost:5000/question/${question}`)}
+    catch (error) {
+        console.error(error)
+    }
     let data = await response.json()
     chat.innerHTML = `<p class="left">${data["response"]}</p>`+ chat_save
+    chat.innerHTML = `<div id="map"></div>` + chat.innerHTML
+    initMap(data["latitude"], data["longitude"])
+}
+
+let map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+  });
 }
