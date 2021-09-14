6f01a34a40a5fe6820a6e75cf0931c29828e88b6
digoin <titouan.dechanet@teamwork.net>
2021-09-14 10:20:24 +0200

    comments + black

--- application/static/script.js
+++ application/static/script.js
@@ -1,3 +1,4 @@
+// this prevent the form to reload the page when you focus it and press enter
 var input = document.getElementById("question");
 input.addEventListener("keyup", function(event) {
     if (event.keyCode === 13) {
@@ -6,20 +7,30 @@ input.addEventListener("keyup", function(event) {
     }
 });
 
+// this function is the main of the page and have multiple uses
 let ask = async function() {
     question = document.getElementById("question").value
     const chat = document.getElementById("chat")
+
+    // we put a loader when grandpy is loading the response
     chat.innerHTML = `<p class="right">${question}</p>`+chat.innerHTML
     chat_save = chat.innerHTML
     chat.innerHTML = `<img class="dots" src="static/image/3dots.gif">`+chat_save
-    let response = await fetch(`http://localhost:5000/question/${question}`)
 
+    // the function call the question function from views.py
+    let response = await fetch(`http://localhost:5000/question/${question}`)
     let data = await response.json()
+
+    // there we get rid of the loader and put the repsonse of the api and a map tag
     chat.innerHTML = `<p class="left">${data["response"]}</p>`+ chat_save
     chat.innerHTML = `<div id="map"></div>` + chat.innerHTML
+
+    // we call the function of google maps API
     initMap(data["latitude"], data["longitude"])
 }
 
+
+// this part is a code gave by google the google maps API to display a map
 let map;
 
 function initMap(latitude, longitude) {
--- application/views.py
+++ application/views.py
@@ -31,16 +31,20 @@ def question(form_data):
     geoencoder = Geoencoder()
     wikipedia = Wikipedia()
 
+    # we pass the request through filters so maps can understand the request
     punctuation_filtered = data_filter.punctuation(form_data)
     filtered = data_filter.stopwords(punctuation_filtered)
 
+    # we ask the request to the google maps API
     geo_response = geoencoder.get_response(filtered)
     location = geoencoder.parse_address(geo_response)
 
+    # we use the google maps API repsponse and use it on wikipedia API
     location["address"] = data_filter.abbreviation(location["address"])
     address_dict = wikipedia.research_address(location)
     title = wikipedia.research_address_title(address_dict)
 
+    # if wikipedia dont find anything about the address, GrandpyBOT will say it
     if title is None:
         address = location["address"]
         response = json.dumps(
@@ -48,15 +52,23 @@ def question(form_data):
                 "response": f"Je n'ai pas d'anecdote sur \"{address}\" à te raconter mon enfant.",
                 "status": "empty",
                 "latitude": location["latitude"],
-                "longitude": location["longitude"]
-                
+                "longitude": location["longitude"],
             }
         )
         return response
+
+    # we set some variable so js can use it
     place_dict = wikipedia.research_place(title)
     extract = wikipedia.research_place_extract(place_dict)
     data = wikipedia.correction(extract)
-    response = json.dumps({"response": data, "status": "ok", "latitude": location["latitude"], "longitude": location["longitude"]})
+    response = json.dumps(
+        {
+            "response": data,
+            "status": "ok",
+            "latitude": location["latitude"],
+            "longitude": location["longitude"],
+        }
+    )
 
     return response
 
