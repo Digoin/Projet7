67b98a6d3bec823d2aa0dda5f4162ab0f8f15195
digoin <titouan.dechanet@teamwork.net>
2021-10-04 17:02:26 +0200

    changed host

--- application/static/script.js
+++ application/static/script.js
@@ -16,7 +16,7 @@ let ask = async function() {
     chat.innerHTML = `<p class="left, loader" id=loader></p><p class="right">${question}</p>`+chat.innerHTML
 
     // the function call the question function from views.py
-    let response = await fetch(`http://localhost:8003/question/${question}`)
+    let response = await fetch(`http://https://powerful-plateau-08757.herokuapp.com/question/${question}`)
     let data = await response.json()
 
     //
